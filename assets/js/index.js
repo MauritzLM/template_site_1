d5d6d85ca0ab0f04c0a23d34e16abd4679a9bf72
Mauritz Marais <mauritz303@gmail.com>
2024-01-09 16:06:21 +0200

    added eventlistener to faq items to toggle class on answer element and img src

--- assets/js/index.js
+++ assets/js/index.js
@@ -15,3 +15,20 @@ document.querySelector(".mobile-nav-button").addEventListener('click', (e) => {
 document.querySelector(".close-nav-button").addEventListener('click', () => {
     mobileNav.classList.toggle("show-menu");
 });
+
+
+// FAQ toggle
+// select elements
+const faqItems = document.querySelectorAll(".faq-item");
+const images = document.querySelectorAll(".faq-item img");
+const answers = document.querySelectorAll(".faq-item .answer");
+
+// loop over array and add eventlistener
+for (let i = 0; i < faqItems.length; i++) {
+      faqItems[i].addEventListener("click", (e) => {
+        // toggle hidden class on answer
+        answers[i].classList.toggle("hidden");
+        // update image src
+        answers[i].classList.contains("hidden") ? images[i].src = "../assets/icons/add.svg" : images[i].src = "../assets/icons/remove.svg";
+      });
+};
