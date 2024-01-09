8e519517af63565062254990449560898827d2e1
Mauritz Marais <mauritz303@gmail.com>
2024-01-09 16:09:20 +0200

    updated footer styles, product page layout, added more icons

--- _site/assets/js/index.js
+++ _site/assets/js/index.js
@@ -15,3 +15,19 @@ document.querySelector(".mobile-nav-button").addEventListener('click', (e) => {
 document.querySelector(".close-nav-button").addEventListener('click', () => {
     mobileNav.classList.toggle("show-menu");
 });
+
+
+// FAQ toggle
+
+const faqItems = document.querySelectorAll(".faq-item");
+const images = document.querySelectorAll(".faq-item img");
+const answers = document.querySelectorAll(".faq-item .answer");
+
+for (let i = 0; i < faqItems.length; i++) {
+      faqItems[i].addEventListener("click", (e) => {
+        answers[i].classList.toggle("hidden");
+        // update image src*
+        answers[i].classList.contains("hidden") ? images[i].src = "../assets/icons/add.svg" : images[i].src = "../assets/icons/remove.svg";
+        console.log(e);
+      });
+};
